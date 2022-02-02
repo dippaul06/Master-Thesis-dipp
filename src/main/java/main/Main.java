aef70602066b16dc74542b750936de9de0fc00f9
daniels <daniels@simula.no>
2022-02-02 18:07:12 +0100

    chaged dependencies

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import transform.Transformer;
+
+import java.nio.file.Path;
+
+// THIS IS THE MAIN CLASS FOR LOCAL
+// TESTING FROM THE IDE.
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Path path = Path.of("/global/D1/projects/umod/dipp/");
+//        new Transformer().loadXzDump(path);
+    }
+}
