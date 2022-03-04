9c5a51cb7959cc408eb7789b3cd5fa2b742e2747
Daniel Thilo Schroeder <daniel.schroeder25@gmail.com>
2022-03-04 11:30:38 +0100

    wip

--- src/main/java/model/Location.java
+++ src/main/java/model/Location.java
@@ -1,14 +1,42 @@
 package model;
 
-public class Location {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
-    final String name;
-    final double xCoordinate;
-    final double yCoordinate;
+// Location Database
+public enum Location {
+    get;
 
-    public Location(String name, double xCoordinate, double yCoordinate) {
-        this.name = name;
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
+
+    Map<Long, String> uid_Location = new HashMap<>();
+    Map<String, String> Location_Location = new HashMap<>();
+
+
+
+    void readLocations() {
+        // id, userLocation, goggle_location([xxx, xxx, xxx, xxx])
+        // for over all files
+        // --- add id, userLocation, goggle_location([xxx, xxx, xxx, xxx]) to uid_Location & Location_Location
     }
+
+    Optional<String> getLocationFromId(long userId) {
+
+    }
+
+    Optional<String> getLocationFromLocation(long userId) {
+
+    }
+
+
+
+//    final String name;
+//    final double xCoordinate;
+//    final double yCoordinate;
+//
+//    public Location(String name, double xCoordinate, double yCoordinate) {
+//        this.name = name;
+//        this.xCoordinate = xCoordinate;
+//        this.yCoordinate = yCoordinate;
+//    }
 }
